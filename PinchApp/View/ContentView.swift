d46cdbbf1886fb5a3ac0321d41cd49b391ed8950
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-09-07 02:14:04 +0700

    Add DragGesture with snapback

--- PinchApp/View/ContentView.swift
+++ PinchApp/View/ContentView.swift
@@ -10,6 +10,28 @@ import SwiftUI
 struct ContentView: View {
   @State private var isAnimating = false
   @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero
+
+  func resetImageState() {
+    withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
+
+  var dragGesture: some Gesture {
+    DragGesture()
+      .onChanged { value in
+        withAnimation(.linear(duration: 1)) {
+          imageOffset = value.translation
+        }
+      }
+      .onEnded { _ in // no need for .translation
+        if imageScale <= 1 {
+          resetImageState()
+        }
+      }
+  }
 
     var body: some View {
       NavigationStack {
@@ -21,18 +43,20 @@ struct ContentView: View {
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
           .opacity(isAnimating ? 1:0)
+          .offset(x: imageOffset.width, y: imageOffset.height)
           .scaleEffect(imageScale)
+          //MARK: - Double tap gesture
           .onTapGesture(count: 2) {
             if imageScale == 1 {
               withAnimation(.spring()) {
                 imageScale = 5
               }
             } else {
-              withAnimation(.spring()) {
-                imageScale = 1
-              }
+              resetImageState()
             }
           }
+          //MARK: - Drag gesture
+          .gesture(dragGesture)
           .animation(.linear(duration: 1), value: isAnimating)
         } // ZS
         .navigationTitle("Pinch & Zoom")
