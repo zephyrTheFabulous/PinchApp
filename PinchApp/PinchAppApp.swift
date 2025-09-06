483660710ff2529570ca83883cf825b18c4cd3ad
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-09-06 23:30:10 +0700

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Anthony on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
