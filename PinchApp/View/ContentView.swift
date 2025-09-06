aecd3a7630358d714c9f928e81b0752faf549650
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-09-06 23:50:03 +0700

    Add fade-in animation to Image

--- PinchApp/View/ContentView.swift
+++ PinchApp/View/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var isAnimating = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      NavigationStack {
+        ZStack {
+          Image("magazine-front-cover")
+            .resizable()
+            .scaledToFit()
+          .clipShape(.rect(cornerRadius: 10))
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1:0)
+          .animation(.linear(duration: 1), value: isAnimating)
+        } // ZS
+        .navigationTitle("Pinch & Zoom")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+          isAnimating = true
         }
-        .padding()
+      } //: NAV
     }
 }
 
