7a639a2aab0499b31669e6be479703bc672768a8
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-09-06 23:54:26 +0700

    Add double-tap gesture

--- PinchApp/View/ContentView.swift
+++ PinchApp/View/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var isAnimating = false
+  @State private var imageScale: CGFloat = 1
 
     var body: some View {
       NavigationStack {
@@ -20,6 +21,18 @@ struct ContentView: View {
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
           .opacity(isAnimating ? 1:0)
+          .scaleEffect(imageScale)
+          .onTapGesture(count: 2) {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
+              }
+            } else {
+              withAnimation(.spring()) {
+                imageScale = 1
+              }
+            }
+          }
           .animation(.linear(duration: 1), value: isAnimating)
         } // ZS
         .navigationTitle("Pinch & Zoom")
