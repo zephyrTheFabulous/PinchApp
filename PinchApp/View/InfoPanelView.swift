2ccc1f94f16f6dd2b626c6ff7e9badd3de915a73
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-09-07 17:37:03 +0700

    Add Info panel showing image drag coordinates

---
+++ PinchApp/View/InfoPanelView.swift
@@ -0,0 +1,52 @@
+//
+//  InfoPanelView.swift
+//  PinchApp
+//
+//  Created by Anthony on 7/9/25.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+  var scale: CGFloat
+  var offset: CGSize
+
+  var body: some View {
+    HStack {
+      Image(systemName: "smallcircle.circle")
+        .symbolRenderingMode(.hierarchical)
+        .resizable()
+//        .scaledToFit()
+        .frame(width: 30, height: 30)
+
+      Spacer()
+      HStack (spacing: 2) {
+        Image(systemName: "arrow.up.left.and.arrow.down.right")
+        Text("\(scale)")
+
+        Spacer()
+
+        Image(systemName: "arrow.left.and.right")
+        Text("\(offset.width)")
+
+        Spacer()
+
+        Image(systemName: "arrow.up.and.down")
+        Text("\(offset.height)")
+      }
+      .font(.footnote)
+      .padding(8)
+      .background(.ultraThinMaterial)
+      .clipShape(.rect(cornerRadius: 8))
+      .frame(maxWidth: 420)
+
+      Spacer()
+    } //: main HS
+  }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+  InfoPanelView(scale: 1, offset: .zero)
+    .preferredColorScheme(.dark)
+    .padding()
+}
